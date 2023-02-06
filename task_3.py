9a4fde5e4ce78bdc631cce211e61ec43682e2b0f
gooder08 <118513778+gooder08@users.noreply.github.com>
2023-02-06 12:14:02 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -2,13 +2,16 @@ f1 = open('1.txt', 'rt', encoding='utf-8')
 f2 = open('2.txt', 'rt', encoding='utf-8')
 f3 = open('3.txt', 'rt', encoding='utf-8')
 final = open('final.txt', 'w', encoding='utf-8')
+final_list = []
 a = f1.readlines()
 b = f2.readlines()
 c = f3.readlines()
+final_list.append(f'{f1.name}\n{str(len(a))}\n{"".join(a)}\n')
+final_list.append(f'{f2.name}\n{str(len(b))}\n{"".join(b)}\n')
+final_list.append(f'\n{f3.name}\n{str(len(c))}\n{"".join(c)}\n')
+final_list.sort(key=len)
+final.write("".join(final_list))
 
-final.write(f'{f2.name}\n{len(b)}\n{"".join(b)}\n'
-            f'{f1.name}\n{len(a)}\n{"".join(a)}\n\n'
-            f'{f3.name}\n{len(c)}\n{"".join(c)}')
 
 f1.close()
 f2.close()
